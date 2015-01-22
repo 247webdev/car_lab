e37426367032bd1de101eb0f46b96dd2c64f6bdb
Christian Chandler <247webdev+github@googlemail.com>
2015-01-21 22:39:26 -0800

    initial push of lab

--- src/car.js
+++ src/car.js
@@ -1,13 +1,58 @@
-function Car(make, model, year, color){
-
+function Car(make, model, year, state, color){
+	this.make	= make;
+	this.module = model;
+	this.year	= year;
+	this.state	= "off";
+	this.color	= color;
+	this.previous_owners = [];
+	this.current_owner	= "Manufacturer";
+	this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner){
-
+	this.previous_owners.push(this.current_owner);
+	this.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
+
+Car.prototype.start = function(){
+	this.state = "on";
+};
+
+Car.prototype.off = function(){
+	this.state = "off";
+};
+
+Car.prototype.driveTo = function(destination){
+	if( this.state === "on" ){
+		console.log("driving to " + destination );
+	}
+};
+
+Car.prototype.park = function(){
+	if( this.state === "off" ){
+		console.log( "parked!!" );
+	}
+};
+
+Car.prototype.pick_up = function(name){
+	if( this.state === "on" ){
+		console.log("driving to 'pick up " + name + "'");
+		this.passengers.push(name);
+	}
+};
+
+Car.prototype.dropOff = function(name){
+	if( this.state === "on" ){
+		if( this.passengers.indexOf(name) != -1 ){
+			this.passengers.splice( this.passengers.indexOf(name), 1);
+		}
+	}
+};
+
+
 
 module.exports=Car;
